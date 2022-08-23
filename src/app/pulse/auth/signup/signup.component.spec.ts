466013f0707c0318736d8c783fc8fa7b9b5cc16f
Victor Santana <victor.santana@tracknme.com.br>
2022-08-23 10:06:39 -0400

    adicionado teste de html

--- src/app/pulse/auth/signup/signup.component.spec.ts
+++ src/app/pulse/auth/signup/signup.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ToastrModule } from 'ngx-toastr';
 import { HintComponent } from 'src/app/components/hint/hint.component';
@@ -14,9 +15,9 @@ describe('SignupComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, RouterTestingModule, HttpClientModule, ToastrModule.forRoot()],
-      declarations: [ SignupComponent , HintComponent]
+      declarations: [SignupComponent, HintComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -27,5 +28,20 @@ describe('SignupComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should enable submit', () => {
+    const emailElem = fixture.debugElement.query(By.css('#email'));
+    const passwordElem = fixture.debugElement.query(By.css('#password'));
+    const usernameElem = fixture.debugElement.query(By.css('#username'));
+    const fullnameElem = fixture.debugElement.query(By.css('#fullname'));
+    const submitElem = fixture.debugElement.query(By.css('#submit'));
+
+    emailElem.nativeElement.value = 'email@email.com';
+    passwordElem.nativeElement.value = 'asd123';
+    usernameElem.nativeElement.value = 'Lorem';
+    fullnameElem.nativeElement.value = 'Lorem Ipsum';
+
+    expect(submitElem.nativeElement.getAttribute('disabled')).not.toBe(null);
   });
 });
